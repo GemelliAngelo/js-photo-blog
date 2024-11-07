a39be82e26bfcba38cd347c419bc6bbb2a846725
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-07 14:40:15 +0100

    add card text via API

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-const cardText = document.querySelectorAll("card-text");
+const cardTextEl = document.querySelectorAll(".card-text");
 
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((response) => response.json())
-  .then((data) => {});
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      cardTextEl[i].innerText = `${data[i].title}`;
+    }
+  });
