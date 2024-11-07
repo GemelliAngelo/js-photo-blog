ea0f370a146106ea55d9e4933c05531c10b535be
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-07 14:54:21 +0100

    add image via API

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const cardTextEl = document.querySelectorAll(".card-text");
+const cardImageEl = document.querySelectorAll(".card-image");
 
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((response) => response.json())
   .then((data) => {
     for (let i = 0; i < data.length; i++) {
       cardTextEl[i].innerText = `${data[i].title}`;
+      cardImageEl[i].setAttribute(`src`, `${data[i].url}`);
     }
   });
