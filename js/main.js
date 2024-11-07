411db0025b3e802a864c9e7ff345c578f6377936
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-07 15:26:47 +0100

    fix dimension

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((response) => response.json())
   .then((data) => {
     for (let i = 0; i < data.length; i++) {
-      cardTextEl[i].innerText = `${data[i].title}`;
-      cardImageEl[i].setAttribute(`src`, `${data[i].url}`);
+      const album = data[i];
+      const cardText = cardTextEl[i];
+      const cardImage = cardImageEl[i];
+
+      cardText.innerText = `${album.title}`;
+      cardImage.setAttribute(`src`, `${album.url}`);
     }
   });
