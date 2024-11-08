d657a8ff5bb503c77eff364232d75d8a6e510117
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-08 12:04:45 +0100

    add close button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const cardEl = document.querySelectorAll(".card");
 const cardTextEl = document.querySelectorAll(".card-text");
 const cardImageEl = document.querySelectorAll(".card-image");
 const overlayEl = document.querySelector(".overlay");
+const buttonEl = document.querySelector(".btn.btn-outline-info");
 
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((response) => response.json())
@@ -13,10 +14,13 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
 
       cardText.innerText = `${album.title}`;
       cardImage.setAttribute(`src`, `${album.url}`);
-      cardEl.forEach((card) => {
-        card.addEventListener("click", () => {
-          overlayEl.classList.remove("d-none");
-        });
-      });
     }
+    cardEl.forEach((card) => {
+      card.addEventListener("click", () => {
+        overlayEl.classList.remove("d-none");
+      });
+    });
+    buttonEl.addEventListener("click", () => {
+      overlayEl.classList.add("d-none");
+    });
   });
